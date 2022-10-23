6f7546bb4a099934a2414523e7b4756a3320ed18
JulioFornos <juliofornos2@gmail.com>
2022-10-22 23:06:27 -0600

    .

---
+++ onepunch/src/main/java/com/uam/onepunch/repository/IPagoRepository.java
@@ -0,0 +1,8 @@
+package com.uam.onepunch.repository;
+import com.uam.onepunch.model.Pago;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPagoRepository extends JpaRepository<Pago, Long>{
+
+}
+
