15de25ad0f252b41a0c1e2887245c1bba0be207f
JulioFornos <juliofornos2@gmail.com>
2022-10-22 19:39:04 -0600

    Se creo Orden,IServiceOrden,impServiceOrden y IOrdenRepository

---
+++ onepunch/src/main/java/com/uam/onepunch/service/impServiceOrden.java
@@ -0,0 +1,33 @@
+package com.uam.onepunch.service;
+
+import com.uam.onepunch.model.Orden;
+import com.uam.onepunch.repository.IOrdenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class impServiceOrden {
+    @Autowired
+    private IOrdenRepository repoOrd;
+
+    @Override
+    public List<Orden> getListOrden(){
+        return repoOrd.findAll();
+    }
+
+    @Override
+        public Orden findById(Long idorden) {
+        return repoOrd.findById(idorden).get()  ;
+    }
+
+    @Override
+    public Orden saveOrden(Orden orden){
+        return repoOrd.save(orden);
+    }
+
+    @Override
+    public void deleteOrden(Long idorden) {
+        repoOrd.deleteById(idorden);
+    }
+
+}
