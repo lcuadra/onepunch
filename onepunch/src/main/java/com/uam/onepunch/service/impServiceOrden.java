498d9ac49b57707eb00fab6b162bf907db150f2d
HerzigJonathan <50646203+CodeRyuta0@users.noreply.github.com>
2022-10-22 20:17:28 -0600

    Arreglado Override impServiceOrden

--- onepunch/src/main/java/com/uam/onepunch/service/impServiceOrden.java
+++ onepunch/src/main/java/com/uam/onepunch/service/impServiceOrden.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public class impServiceOrden {
+public class impServiceOrden implements IServiceOrden {
     @Autowired
     private IOrdenRepository repoOrd;
 
